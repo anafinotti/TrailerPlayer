f38a490699bbd4b69d3ecd9b4b1d89d0c93a8715
Ana Finotti <ana.finotti@zflow.com.br>
2021-01-25 20:35:15 +0000

    Initial Commit

---
+++ TrailerPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrailerPlayer
+//
+//  Created by Ana Finotti on 25/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
